4c180c7253522b1baeaa9402dc1527afa16c1160
siddhumanka <siddhu.manka94@gmail.com>
2015-06-23 10:26:59 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 n=int(raw_input("Enter total number of nodes"))
 j=0
 f=open("1.txt")
@@ -20,6 +22,29 @@ def admatrix():
 		c[a[0]-1][a[1]-1]=(a[2])
 		c[a[1]-1][a[0]-1]=(a[2])
 	print c
+	f.close()
+	bfs(c)
+	#dfs() 
+
+def adlist():
+	dict1=defaultdict(dict)
+	dict2={}
+	a=[0,0,0] 
+	f=open("1.txt")
+	for w in f:
+		l=0
+		for i in w.split(' '):
+			a[l]=int(i)
+			l=l+1
+		#dict2[a[1]]=a[2]
+		dict1[a[0]][a[1]]=a[2]
+		dict2={}
+	print dict1
+	#bfs()
+	#dfs()
+
+def bfs(c = [[0 for x in range(n)] for x in range(n)]):
+	
 
 if (rt<float(cn)):
 	print "Using list"
